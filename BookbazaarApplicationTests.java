3d2c4357370e20f67a8e6de9f71023c18a9da3cd
Shivaprasad G Bhat <137981972+shivagbhat@users.noreply.github.com>
2024-06-26 14:38:59 +0530

    Add files via upload

---
+++ BookbazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectfirst.bookbazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookbazaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
